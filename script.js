1093999be85f19d8f3a5f5862604e1e44453c315
Abdelrahman Mohamadeen <33037931+Abdo-A@users.noreply.github.com>
2018-01-04 09:58:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ audio_icon.addEventListener("click",function(){
 else
   audio.play();
 });
+
+  document.getElementById("ima").style.animation="opac1 2s 4.8s forwards";
+
+  document.getElementById("desc").style.animation="opac1 2s 5.8s forwards";
+  
+  setTimeout(function(){document.getElementById("learn").style.display="block";},14500);
+
+setTimeout(function(){
+  headeranimations();
+  setInterval(headeranimations,14500);
+},7500);
+
+  function headeranimations() {
+    var fh= document.getElementsByClassName("fh")[0];
+    var sh= document.getElementsByClassName("sh")[0];
+    fh.style.animation="opac0 0.5s 1s forwards";
+    setTimeout(function(){fh.style.animation="opac1 1.5s forwards";fh.innerHTML="Software";},1500);
+    setTimeout(function(){sh.style.animation="opac0 0.5s forwards";},2000);
+    setTimeout(function(){sh.style.animation="opac1 1.5s forwards";sh.innerHTML="Engineer";},2500);
+    setTimeout(function(){fh.style.animation="opac0 0.5s forwards";},6000);
+    setTimeout(function(){fh.style.animation="opac1 1.5s forwards";fh.innerHTML="Front End";},6500);
+    setTimeout(function(){sh.style.animation="opac0 0.5s forwards";},7000);
+    setTimeout(function(){sh.style.animation="opac1 1.5s forwards";sh.innerHTML="Specialist";},7500);
+    setTimeout(function(){fh.style.animation="opac0 0.5s forwards";},11000);
+    setTimeout(function(){fh.style.animation="opac1 1.5s forwards";fh.innerHTML="Web";},11500);
+    setTimeout(function(){sh.style.animation="opac0 0.5s forwards";},12000);
+    setTimeout(function(){sh.style.animation="opac1 1.5s forwards";sh.innerHTML="Developer";},12500);
+}
